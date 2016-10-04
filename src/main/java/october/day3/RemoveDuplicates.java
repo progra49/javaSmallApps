9afebe63a94db6f4d5f9f8a653f05474ce237de8
Courious Programmer <blog@programmerscuriosity.com>
2016-10-04 12:27:23 +0100

    initial commit with one simple app

---
+++ src/main/java/october/day3/RemoveDuplicates.java
@@ -0,0 +1,44 @@
+package october.day3;
+
+import java.util.ArrayList;
+
+/**
+ * Created by marom on 03/10/16.
+ */
+public class RemoveDuplicates {
+
+    //Write a Java program to remove duplicate elements from an arraylist
+    // without using collections (without using set)
+
+    public static void main(String[] args) {
+
+        ArrayList<Object> al = new ArrayList<>();
+
+        al.add("java");
+        al.add('a');
+        al.add('b');
+        al.add('a');
+        al.add("java");
+        al.add(10.3);
+        al.add('c');
+        al.add(14);
+        al.add("java");
+        al.add(12);
+
+        System.out.println("Before removing duplicates");
+        al.stream().forEach(System.out::println);
+
+        for (int i = 0; i < al.size(); i++) {
+            for (int j = i +i; j < al.size(); j++) {
+                if(al.get(i).equals(al.get(j))) {
+                    al.remove(j);
+                    j--;
+                }
+            }
+        }
+
+        System.out.println("After removing duplicates");
+        al.stream().forEach(System.out::println);
+
+    }
+}
