1abafbba741c085e6b3cd4c251dc89d7c722a5a2
Courious Programmer <blog@programmerscuriosity.com>
2016-10-04 13:28:57 +0100

    add duplicate filtering using steams

--- src/main/java/october/day3/RemoveDuplicates.java
+++ src/main/java/october/day3/RemoveDuplicates.java
@@ -1,6 +1,9 @@
 package october.day3;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Created by marom on 03/10/16.
@@ -12,21 +15,11 @@ public class RemoveDuplicates {
 
     public static void main(String[] args) {
 
-        ArrayList<Object> al = new ArrayList<>();
+        ArrayList<String> al = createArray();
 
-        al.add("java");
-        al.add('a');
-        al.add('b');
-        al.add('a');
-        al.add("java");
-        al.add(10.3);
-        al.add('c');
-        al.add(14);
-        al.add("java");
-        al.add(12);
-
-        System.out.println("Before removing duplicates. Size: " + al.size());
-        al.stream().forEach(System.out::println);
+        System.out.print("Before removing duplicates. Size: " + al.size()  + " ");
+        al.stream().forEach(s -> System.out.print(s + " "));
+        System.out.println();
 
         for (int i = 0; i < al.size(); i++) {
             for (int j = i +1; j < al.size(); j++) {
@@ -37,8 +30,30 @@ public class RemoveDuplicates {
             }
         }
 
-        System.out.println("After removing duplicates. Size: " + al.size());
-        al.stream().forEach(System.out::println);
+        System.out.print("After removing duplicates. Size: " + al.size() + " ");
+        al.stream().forEach(s -> System.out.print(s + " "));
+        System.out.println();
 
+        al = createArray();
+        List<String> listWithoutDuplicates = al.stream().distinct().collect(Collectors.toList());
+        System.out.print("After removing duplicates. Size: " + listWithoutDuplicates.size() + " ");
+        listWithoutDuplicates.stream().forEach(s -> System.out.print(s + " "));
+
+    }
+
+    private static ArrayList<String> createArray() {
+        ArrayList<String> al = new ArrayList<>();
+
+        al.add("java");
+        al.add("a");
+        al.add("b");
+        al.add("a");
+        al.add("java");
+        al.add("10.3");
+        al.add("c");
+        al.add("14");
+        al.add("java");
+        al.add("12");
+        return al;
     }
 }
